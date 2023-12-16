1bbac30acfcf42766982d1e9fdc277b4e625b013
Gustavo <75509324+guskpo20@users.noreply.github.com>
2023-12-16 13:58:30 -0300

    presentation styles fixed

--- src/app/components/presentation/Presentation.jsx
+++ src/app/components/presentation/Presentation.jsx
@@ -22,17 +22,22 @@ function Presentation() {
     }, [userLang])
     
   return (
+    <>
     <section className={`${styles.presentation} ${isDarkMode ? styles.dark : styles.light}`}>
-      <Image src={fotoPerfil} width={350} height={350} alt={"fotoGustavo"} className={`${styles.profilePhoto}`}/>
       <div>
-        <h1 className={`${styles.name}`}>Gustavo Keymetlian</h1>
-        <p className={`${styles.description}`}>{texts.description}</p>
-        <div className={`${styles.flex}`}>
-        <Image src={dev} width={35} height={35} alt="download image"/>
-        <a className={`${styles.downloadSection} ${ userLang == "en" ? styles.english : "" }`} href={ userLang == "en" ? "/GustavoKeymetlianEnglish.pdf" : "/GustavoKeymetlian.pdf"} download>{texts.download}<Image src={downloadImg} width={15} height={15} alt="download image"/></a>
+        <Image src={fotoPerfil} width={350} height={350} alt={"fotoGustavo"} className={`${styles.profilePhoto}`}/>
+        <div>
+          <h1 className={`${styles.name}`}>Gustavo Keymetlian</h1>
+          <p className={`${styles.description}`}>{texts.description}</p>
+          <div className={`${styles.flex}`}>
+          <Image src={dev} width={35} height={35} alt="download image"/>
+          <a className={`${styles.downloadSection} ${ userLang == "en" ? styles.english : "" }`} href={ userLang == "en" ? "/GustavoKeymetlianEnglish.pdf" : "/GustavoKeymetlian.pdf"} download>{texts.download}<Image src={downloadImg} width={15} height={15} alt="download image"/></a>
+          </div>
         </div>
       </div>
+     <hr />
     </section>
+    </>
   )
 }
 
